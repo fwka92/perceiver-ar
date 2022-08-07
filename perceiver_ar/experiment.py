a18f7b55ce6a70dfb03019e7df9d6aae632ee495
fwcwmc <florian.wild@cwmc.best>
2022-08-07 18:39:31 +0200

    Revert import

--- perceiver_ar/experiment.py
+++ perceiver_ar/experiment.py
@@ -26,7 +26,7 @@ from typing import Mapping, Optional, Text, Tuple
 from absl import app
 from absl import flags
 from absl import logging
-from ..perceiver_ar import dataset
+from perceiver_ar import dataset
 import dill
 import haiku as hk
 import jax
@@ -39,7 +39,7 @@ from perceiver_ar import losses
 from ml_collections import config_dict
 import numpy as np
 import optax
-from ..perceiver_ar import perceiver_ar_model
+from perceiver_ar import perceiver_ar_model
 import tensorflow as tf
 
 FLAGS = flags.FLAGS
